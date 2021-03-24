53610d2550b74cd7867249ce8dc3a2d33094b9af
Darrin Duncan <darrin@darrinduncan.com>
2021-03-23 21:16:48 -0400

    Added js interaction

---
+++ app.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    $(".overlay").show();
+
+    $("#hide").click(function () {
+        $(".overlay").hide();
+    });
+    $("#show").click(function () {
+        $(".overlay").show();
+    });
+
+    $("#first").click(function () {
+        $(".overlay").show();
+        let $newTitle = $("#firstTitle").text()
+        let $newAmt = $("#firstAmt").text()
+        $(".overlay h4").text($newTitle);
+        $(".overlay .amt").text($newAmt);
+    });
+
+    $("#second").click(function () {
+        $(".overlay").show();
+        let $newTitle = $("#secondTitle").text()
+        let $newAmt = $("#secondAmt").text()
+        $(".overlay h4").text($newTitle);
+        $(".overlay .amt").text($newAmt);
+    });
+
+    $("#third").click(function () {
+        $(".overlay").show();
+        let $newTitle = $("#thirdTitle").text()
+        let $newAmt = $("#thirdAmt").text()
+        $(".overlay h4").text($newTitle);
+        $(".overlay .amt").text($newAmt);
+    });
+});
